483c8b8e8de279c9a5fb40f60a307d00508d6af6
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-03-09 10:54:13 +0000

    Add AMS plots by time of day

--- York_Beijing_met_AQ.py
+++ York_Beijing_met_AQ.py
@@ -35,20 +35,19 @@ df_merge_filtime["DateTime"] =pd.to_datetime(df_merge_filtime["date_mid"])
 df_merge_filtime.set_index(pd.to_datetime(df_merge_filtime["date_mid"]),inplace=True)
 df_merge_filtime['date_start'] = pd.to_datetime(df_merge_filtime['date_start'])
 df_merge_filtime['date_end'] = pd.to_datetime(df_merge_filtime['date_end'])
+#df_merge_filtime['time_cat'] = pd.Categorical(delhi_calc_time_cat(df_merge_filtime),[ 'Night', 'Morning','Midday' ,'Afternoon'], ordered=True)
+df_merge_filtime['time_cat'] = pd.Categorical(delhi_calc_time_cat(df_merge_filtime),['Morning','Midday' ,'Afternoon','Night'], ordered=True)
+
+#time_cat_order = pd.Categorical([ 'Night', 'Morning','Midday' ,'Afternoon'], ordered=True)
 
 
 
-
-df_merge_filtime['4time'] = delhi_calc_4time(df_merge_filtime)
-
-
-
-#%%Gantt chart of filters
+#%%Gantt chart of filters- does not work!
 sample_num = pd.Series(range(1, df_merge_filtime.shape[0]+1))
 sample_num.index = df_merge_filtime.index
 df_merge_filtime['sample_num'] = sample_num
 fig, ax = plt.subplots(1, figsize=(16,6))
-ax.barh(sample_num, df_merge_filtime['sample_time_h'], left=df_merge_filtime['date_start'])
+ax.barh(sample_num, df_merge_filtime['time_length_h'], left=df_merge_filtime['date_start'])
 plt.show()
 
 
@@ -206,22 +205,93 @@ ax[1].set(xlabel="")
 ax[2].set(xlabel="")
 
 
+#%%Gas phase compared to time_cat
+fig, ax = plt.subplots(3,1,figsize=(10,7),sharex=True)
+fig.suptitle('gas phase weekday')
+sns.boxplot(data=df_merge_filtime, x=df_merge_filtime['time_cat'], y="co_ppbv", ax=ax[0])
+sns.boxplot(data=df_merge_filtime, x=df_merge_filtime['time_cat'], y="nox_ppbv",ax=ax[1])
+sns.boxplot(data=df_merge_filtime, x=df_merge_filtime['time_cat'], y="o3_ppbv",ax=ax[2])
+ax[0].set(ylim=(0, 1500))
+ax[1].set(ylim=(0, 100))
+ax[0].set(xlabel="")
+ax[1].set(xlabel="")
+ax[2].set(xlabel="")
 
 
-# %%Munge 1min data for diurnal analysis
-df_merge_1min_diurnal = df_merge_1min.groupby(df_merge_1min.index.time).describe()
-df_merge_1min_diurnal.index = pd.to_datetime(df_merge_1min_diurnal.index.astype(str))
 
 
-#%%
-fig,ax = plt.subplots(2,1)
+# %%Munge AMS data for time_cat analysis
+df_merge_time_cat_stats = df_merge_filtime.groupby(df_merge_filtime['time_cat']).describe()
+
+df_merge_ams_cat_mean = df_merge_filtime[['NO3_ams','Org_ams','SO4_ams','NH4_ams','Chl_ams']].groupby(df_merge_filtime['time_cat']).mean()
+df_merge_ams_cat_std = df_merge_filtime[['NO3_ams','Org_ams','SO4_ams','NH4_ams','Chl_ams']].groupby(df_merge_filtime['time_cat']).std()
+df_merge_ams_cat_mean_norm = df_merge_ams_cat_mean.div(df_merge_ams_cat_mean.sum(axis=1),axis=0)
+
+
+df_merge_amspmf_cat_mean = df_merge_filtime[['HOA_ams','COA_ams','OOA1_ams','OOA2_ams','OOA3_ams']].groupby(df_merge_filtime['time_cat']).mean()
+df_merge_amspmf_cat_std = df_merge_filtime[['HOA_ams','COA_ams','OOA1_ams','OOA2_ams','OOA3_ams']].groupby(df_merge_filtime['time_cat']).std()
+df_merge_amspmf_cat_mean_norm = df_merge_amspmf_cat_mean.div(df_merge_amspmf_cat_mean.sum(axis=1),axis=0)
+
+
+#df_merge_time_cat_stats = pd.Categorical(df_merge_time_cat_stats,)
+
+#df_merge_time_cat_stats.sort_values('time_cat',order=time_cat_order)#,inplace=True)
+
+
+#%%AMS Line plot
+fig,ax = plt.subplots(2,1,figsize=(7,10))
 ax1=ax[0]
 ax2=ax[1]
 
-ax1.plot(df_merge_1min_diurnal.index, df_merge_1min_diurnal['NO3_ams']['mean'], linewidth=2.0,c='b',label='NO3')
-ax1.plot(df_merge_1min_diurnal.index, df_merge_1min_diurnal['Org_ams']['mean'], linewidth=2.0,c='g',label='Org')
-ax1.plot(df_merge_1min_diurnal.index, df_merge_1min_diurnal['SO4_ams']['mean'], linewidth=2.0,c='r',label='SO4')
-ax1.plot(df_merge_1min_diurnal.index, df_merge_1min_diurnal['NH4_ams']['mean'], linewidth=2.0,c='o',label='NH4')
-ax1.plot(df_merge_1min_diurnal.index, df_merge_1min_diurnal['Chl_ams']['mean'], linewidth=2.0,c='pink',label='Chl')
+ax1.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['NO3_ams']['mean'], linewidth=5,c='b',label='NO3')
+ax1.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['Org_ams']['mean'], linewidth=5,c='lime',label='Org')
+ax1.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['SO4_ams']['mean'], linewidth=5,c='r',label='SO4')
+ax1.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['NH4_ams']['mean'], linewidth=5,c='orange',label='NH4')
+ax1.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['Chl_ams']['mean'], linewidth=5,c='pink',label='Chl')
+ax1.set_ylabel('µg m$^{-3}$')
+ax1.set_ylim(0,)
+ax1.legend(bbox_to_anchor=(1.05, 0.75))
+
+ax2.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['HOA_ams']['mean'], linewidth=5,c='k',label='HOA')
+ax2.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['COA_ams']['mean'], linewidth=5,c='brown',label='COA')
+ax2.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['OOA1_ams']['mean'], linewidth=5,c='hotpink',label='OOA1',marker='o')
+ax2.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['OOA2_ams']['mean'], linewidth=5,c='deeppink',label='OOA2',marker='^')
+ax2.plot(df_merge_time_cat_stats.index, df_merge_time_cat_stats['OOA3_ams']['mean'], linewidth=5,c='mediumvioletred',label='OOA3',marker='s')
+ax2.set_ylabel('µg m$^{-3}$')
+ax2.set_ylim(0,)
+ax2.legend(bbox_to_anchor=(1.05, 0.75))
+
+#%%AMS stack plot
+fig,ax = plt.subplots(2,1,figsize=(7,10))
+ax1=ax[0]
+ax2=ax[1]
+ax1.stackplot(df_merge_ams_cat_mean_norm.index,df_merge_ams_cat_mean_norm['Chl_ams'], df_merge_ams_cat_mean_norm['NH4_ams'],
+              df_merge_ams_cat_mean_norm['SO4_ams'],df_merge_ams_cat_mean_norm['NO3_ams'],
+              df_merge_ams_cat_mean_norm['Org_ams'], labels=['Chl','NH4','SO4','NO3','Org'],
+              colors=['pink','orange','r','b','lime'])
+ax1.set_ylabel('Fraction')
+ax1.set_ylim(0,)
+ax1.legend(bbox_to_anchor=(1.35, 0.75))
+
+ax2.stackplot(df_merge_amspmf_cat_mean_norm.index,df_merge_amspmf_cat_mean_norm['HOA_ams'], df_merge_amspmf_cat_mean_norm['COA_ams'],
+              df_merge_amspmf_cat_mean_norm['OOA3_ams'],df_merge_amspmf_cat_mean_norm['OOA2_ams'],
+              df_merge_amspmf_cat_mean_norm['OOA1_ams'], labels=['HOA','COA','OOA3','OOA2','OOA1'],
+              colors=['k','brown','mediumvioletred','deeppink','hotpink'])
+ax2.set_ylabel('Fraction')
+ax2.set_ylim(0,)
+#ax2.legend()
+
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend(reversed(handles), reversed(labels), bbox_to_anchor=(1.35, 0.75))
 
 
+#%%
+fig,ax = plt.subplots(2,1,figsize=(7,10))
+ax1=ax[0]
+ax2=ax[1]
+ax1.stackplot(df_merge_time_cat_stats.index,df_merge_time_cat_stats['HOA_ams']['mean'], labels=['HOA'],colors='k')
+ax1.stackplot(df_merge_time_cat_stats.index,df_merge_time_cat_stats['COA_ams']['mean'], labels=['COA'],colors='brown')
+
+#%%
+
+sns.barplot(x = df_merge_time_cat_stats.index, data = df_merge_time_cat_stats)
