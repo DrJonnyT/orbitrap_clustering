6d3cb91791584f394451c0ddd1e97fc058cd1ad4
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-08-19 17:15:26 +0100

    Add dataset_cat and time_cat

--- Combined_data_clustering.py
+++ Combined_data_clustering.py
@@ -7,121 +7,223 @@ Created on Tue Aug  9 17:00:03 2022
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
 import numpy as np
 #import seaborn as sns
 #import math
+import time
+
+from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.cluster import AgglomerativeClustering
 
 import os
 os.chdir('C:/Work/Python/Github/Orbitrap_clustering')
 from ae_functions import *
+from orbitrap_functions import *
 
 
 
-#%%Load Sari's pre-PMF filtered calibrated data from August 2022
-import h5py
-##E.g. round to nearest multiple of 2 would be rounded to 0, 2, 4, etc
-def round_to_nearest_x_even(num,x):
-    return np.round(num / x) * x
-##E.g. round to nearest multiple of 2 would be rounded to 0, 2, 4, etc
-def round_to_nearest_x_odd(num,x):
-    return np.floor((num) / x) * x +x/2
 
-#Merge peaks with same mz and RT
-def sqrt_sum_squares(x):
-    x = np.array(x)
-    return np.sqrt(np.sum(np.multiply(x,x)))
 
-#def Load_pre_PMF_data(filepath):
-
+#%%Load data from HDF
 filepath = r"C:\Users\mbcx5jt5\Google Drive\Shared_York_Man2\PMF data\ORBITRAP_Data_Pre_PMF.h5"
+df_all_data, df_all_err, ds_all_mz = Load_pre_PMF_data(filepath)
 
-with h5py.File(filepath, 'r') as hf:
-    Beijing_winter_mz = np.array(hf['Beijing_Winter']['noNaNs_mz']).astype(float).round(3)
-    Beijing_winter_formula = pd.Series(hf['Beijing_Winter']['noNaNs_Formula']).astype(str).str.replace(r'\'', '',regex=True).str.replace(r'b', '',regex=True)
-    Beijing_winter_RT = pd.Series(hf['Beijing_Winter']['noNaNs_RT_min']).apply(lambda x: round_to_nearest_x_odd(x,1))
-    Beijing_winter_data = np.array(hf['Beijing_Winter']['noNaNs_bjgdata3_wtr'])
-    Beijing_winter_err = np.array(hf['Beijing_Winter']['noNaNs_bjgerr3_wtrPropWk'])
-    Beijing_winter_time = np.array(hf['Beijing_Winter']['noNaNs_midtime_wtr'])
-    df_Beijing_winter_data = pd.DataFrame(Beijing_winter_data,index=Beijing_winter_time)
-    df_Beijing_winter_err = pd.DataFrame(Beijing_winter_data,index=Beijing_winter_time)
+
+#Load all time data, ie start/mid/end
+df_all_times = pd.read_csv(r"C:\Users\mbcx5jt5\Google Drive\Shared_York_Man2\PMF data\Times_all.csv")
+df_all_times['date_start'] = pd.to_datetime(df_all_times['date_start'],dayfirst=True)
+df_all_times['date_mid'] = pd.to_datetime(df_all_times['date_mid'],dayfirst=True)
+df_all_times['date_end'] = pd.to_datetime(df_all_times['date_end'],dayfirst=True)
+df_all_times.set_index(df_all_times['date_mid'],inplace=True)
+df_all_times = df_all_times.loc[df_all_data.index]
+
+dataset_cat = delhi_beijing_datetime_cat(df_all_data)
+df_dataset_cat = pd.DataFrame(delhi_beijing_datetime_cat(df_all_data),columns=['dataset_cat'],index=df_all_data.index)
+ds_dataset_cat = df_dataset_cat['dataset_cat']
+
+time_cat = delhi_calc_time_cat(df_all_times)
+df_time_cat = pd.DataFrame(delhi_calc_time_cat(df_all_times),columns=['time_cat'],index=df_all_times.index)
+ds_time_cat = df_time_cat['time_cat']
+
+
+#%%Test data scaling
+
+#Standard scaling
+standardscaler_all = StandardScaler()
+df_all_data_standard = pd.DataFrame(standardscaler_all.fit_transform(df_all_data.to_numpy()),columns=df_all_data.columns)
+
+standardscaler_all2 = StandardScaler(with_std=False)
+df_all_data_standard2 = pd.DataFrame(standardscaler_all2.fit_transform(df_all_data.to_numpy()),columns=df_all_data.columns)
+
+max_col = df_all_data.mean().idxmax()
+min_col = df_all_data.mean().idxmin()
+
+df_all_data[max_col].plot(kind='hist',title='No scaling, max column')
+df_all_data[min_col].plot(kind='hist',title='No scaling, min column')
+df_all_data_standard[max_col].plot(kind='hist',title='Standard scaling, max column')
+df_all_data_standard[min_col].plot(kind='hist',title='Standard scaling, min column')
+df_all_data_standard2[max_col].plot(kind='hist',title='Standard scaling (not normalised), max column')
+df_all_data_standard2[min_col].plot(kind='hist',title='Standard scaling (not normalised), min column')
+
+#%%Normalise so the mean of the whole matrix is 1
+orig_mean = df_all_data.mean().mean()
+pipe_norm1_mtx = FunctionTransformer(lambda x: np.divide(x,orig_mean),inverse_func = lambda x: np.multiply(x,orig_mean))
+pipe_norm1_mtx.fit(df_all_data)
+df_all_data_norm1 = pd.DataFrame(pipe_norm1_mtx.transform(df_all_data),columns=df_all_data.columns)
+
+
+#%%Setup autoencoder
+df_aug = augment_data_noise(df_all_data_norm1,50,1,0)
+ae_input = df_aug.values
+ae_input_val = df_all_data_norm1.values
+input_dim = ae_input.shape[1]
+
+
+#%%Now compare loss for different latent dimensions
+#This is NOT using kerastuner, and is using log-spaced intermediate layers
+#WARNING THIS TAKES ABOUT HALF AN HOUR
+
+
+latent_dims = []
+AE1_MSE_best50epoch =[]
+AE2_MSE_best50epoch =[]
+AE3_MSE_best50epoch =[]
+AE4_MSE_best50epoch =[]
+best_hps_array = []
+
+
+verbose = 0
+
+start_time = time.time()
+
+for latent_dim in range(1,30):
+    print(latent_dim)
+    K.clear_session()
+    latent_dims.append(latent_dim)
     
-    Beijing_summer_mz = np.array(hf['Beijing_Summer']['noNaNs_mz']).astype(float).round(3)
-    Beijing_summer_formula = pd.Series(hf['Beijing_Summer']['noNaNs_Formula']).astype(str).str.replace(r'\'', '',regex=True).str.replace(r'b', '',regex=True)
-    Beijing_summer_RT = pd.Series(hf['Beijing_Summer']['noNaNs_RT_min']).apply(lambda x: round_to_nearest_x_odd(x,1))
-    Beijing_summer_data = np.array(hf['Beijing_Summer']['noNaNs_bjgdata3_smr'])
-    Beijing_summer_err = np.array(hf['Beijing_Summer']['noNaNs_bjgerr3_smrPropWk'])
-    Beijing_summer_time = np.array(hf['Beijing_Summer']['noNaNs_midtime_smr'])
-    df_Beijing_summer_data = pd.DataFrame(Beijing_summer_data,index=Beijing_summer_time)
-    df_Beijing_summer_err = pd.DataFrame(Beijing_summer_data,index=Beijing_summer_time)
+    #Test for 1 intermediate layer
+    ae_obj = AE_n_layer(input_dim=input_dim,latent_dim=latent_dim,int_layers=1)
+    history = ae_obj.fit_model(ae_input, x_test=ae_input_val,epochs=50,verbose=verbose)
+    val_acc_per_epoch = history.history['val_loss']
+    AE1_MSE_best50epoch.append(min(history.history['val_loss']))
     
-    Delhi_summer_mz = np.array(hf['Delhi_Summer']['noNaNs_mz']).astype(float).round(3)
-    Delhi_summer_formula = pd.Series(hf['Delhi_Summer']['noNaNs_Formula']).astype(str).str.replace(r'\'', '',regex=True).str.replace(r'b', '',regex=True)
-    Delhi_summer_RT = pd.Series(hf['Delhi_Summer']['noNaNs_RT_min']).apply(lambda x: round_to_nearest_x_odd(x,1))
-    Delhi_summer_data = np.array(hf['Delhi_Summer']['noNaNs_dlhdata1_smr'])
-    Delhi_summer_err = np.array(hf['Delhi_Summer']['noNaNs_dlherr1_smrPropWk'])
-    Delhi_summer_time = np.array(hf['Delhi_Summer']['noNaNs_midtime_smr'])
-    df_Delhi_summer_data = pd.DataFrame(Delhi_summer_data,index=Delhi_summer_time)
-    df_Delhi_summer_err = pd.DataFrame(Delhi_summer_data,index=Delhi_summer_time)
+    #Test for 2 intermediate layers
+    ae_obj = AE_n_layer(input_dim=input_dim,latent_dim=latent_dim,int_layers=2)
+    history = ae_obj.fit_model(ae_input, x_test=ae_input_val,epochs=50,verbose=verbose)
+    val_acc_per_epoch = history.history['val_loss']
+    AE2_MSE_best50epoch.append(min(history.history['val_loss']))
     
-    Delhi_autumn_mz = np.array(hf['Delhi_Autumn']['noNaNs_mz']).astype(float).round(3)
-    Delhi_autumn_formula = pd.Series(hf['Delhi_Autumn']['noNaNs_Formula']).astype(str).str.replace(r'\'', '',regex=True).str.replace(r'b', '',regex=True)
-    Delhi_autumn_RT = pd.Series(hf['Delhi_Autumn']['noNaNs_RT_min']).apply(lambda x: round_to_nearest_x_odd(x,1))
-    Delhi_autumn_data = np.array(hf['Delhi_Autumn']['noNaNs_dlhdata1_aut'])
-    Delhi_autumn_err = np.array(hf['Delhi_Autumn']['noNaNs_dlherr1_autPropWk'])
-    Delhi_autumn_time = np.array(hf['Delhi_Autumn']['noNaNs_midtime_aut'])
-    df_Delhi_autumn_data = pd.DataFrame(Delhi_autumn_data,index=Delhi_autumn_time)
-    df_Delhi_autumn_err = pd.DataFrame(Delhi_autumn_data,index=Delhi_autumn_time)
+    #Test for 3 intermediate layers
+    ae_obj = AE_n_layer(input_dim=input_dim,latent_dim=latent_dim,int_layers=3)
+    history = ae_obj.fit_model(ae_input, x_test=ae_input_val,epochs=50,verbose=verbose)
+    val_acc_per_epoch = history.history['val_loss']
+    AE3_MSE_best50epoch.append(min(history.history['val_loss']))
     
-    df_Beijing_winter_data = df_Beijing_winter_data.groupby([Beijing_winter_formula,Beijing_winter_RT],axis=1).aggregate("sum")
-    df_Beijing_winter_err = df_Beijing_winter_err.groupby([Beijing_winter_formula,Beijing_winter_RT],axis=1).aggregate(sqrt_sum_squares)
-    df_Beijing_summer_data = df_Beijing_summer_data.groupby([Beijing_summer_formula,Beijing_summer_RT],axis=1).aggregate("sum")
-    df_Beijing_summer_err = df_Beijing_summer_err.groupby([Beijing_summer_formula,Beijing_summer_RT],axis=1).aggregate(sqrt_sum_squares)
-    df_Delhi_summer_data = df_Delhi_summer_data.groupby([Delhi_summer_formula,Delhi_summer_RT],axis=1).aggregate("sum")
-    df_Delhi_summer_err = df_Delhi_summer_err.groupby([Delhi_summer_formula,Delhi_summer_RT],axis=1).aggregate(sqrt_sum_squares)
-    df_Delhi_autumn_data = df_Delhi_autumn_data.groupby([Delhi_autumn_formula,Delhi_autumn_RT],axis=1).aggregate("sum")
-    df_Delhi_autumn_err = df_Delhi_autumn_err.groupby([Delhi_autumn_formula,Delhi_autumn_RT],axis=1).aggregate(sqrt_sum_squares)
+    #Test for 4 intermediate layers
+    ae_obj = AE_n_layer(input_dim=input_dim,latent_dim=latent_dim,int_layers=4)
+    history = ae_obj.fit_model(ae_input, x_test=ae_input_val,epochs=50,verbose=verbose)
+    val_acc_per_epoch = history.history['val_loss']
+    AE4_MSE_best50epoch.append(min(history.history['val_loss']))
+    
+print("--- %s seconds ---" % (time.time() - start_time))
+    
+#%%Plot the data for the different number of layers above
+fig,ax=plt.subplots(2,1,figsize=(12,8))
+ax[0].set_title('Finding optimum latent dims- simple relu AE')
+ax[0].plot(latent_dims,AE1_MSE_best50epoch,c='black')
+ax[0].plot(latent_dims,AE2_MSE_best50epoch,c='red')
+ax[0].plot(latent_dims,AE3_MSE_best50epoch,c='gray')
+ax[0].plot(latent_dims,AE4_MSE_best50epoch)
+ax[0].set_xlabel('Number of latent dims')
+ax[0].set_ylabel('Best MSE in first 50 epochs')
+ax[1].plot(latent_dims,AE1_MSE_best50epoch,c='black')
+ax[1].plot(latent_dims,AE2_MSE_best50epoch,c='red')
+ax[1].plot(latent_dims,AE3_MSE_best50epoch,c='gray')
+ax[1].plot(latent_dims,AE4_MSE_best50epoch)
+ax[1].set_xlabel('Number of latent dims')
+ax[1].set_ylabel('Best MSE in first 50 epochs')
+ax[1].set_yscale('log')
+ax[1].legend([1,2,3,4],title='Intermediate layers')
+
+
+loc = plticker.MultipleLocator(base=10.0) # this locator puts ticks at regular intervals
+ax[0].xaxis.set_major_locator(plticker.MultipleLocator(base=5.0))
+ax[0].xaxis.set_minor_locator(plticker.MultipleLocator(base=1.0))
+ax[1].xaxis.set_major_locator(plticker.MultipleLocator(base=5.0))
+ax[1].xaxis.set_minor_locator(plticker.MultipleLocator(base=1.0))
+
+plt.show()
+
+
+#%%Work out how many epochs to train for
+#Based on the above, use an AE with 2 intermediate layers and latent dim of 20
+ae_obj = AE_n_layer(input_dim=input_dim,latent_dim=2,int_layers=3)
+history = ae_obj.fit_model(ae_input, x_test=ae_input_val,epochs=100)
+val_acc_per_epoch = history.history['val_loss']
+fig,ax = plt.subplots(1,figsize=(8,6))
+ax.plot(val_acc_per_epoch)
+plt.show()
+#Now retrain model based on best epoch
+best_epoch = val_acc_per_epoch.index(min(val_acc_per_epoch)) + 1
+ae_obj = AE_n_layer(input_dim=input_dim,latent_dim=21,int_layers=2)
+ae_obj.fit_model(ae_input, x_test=ae_input_val,epochs=best_epoch)
+print('Best epoch: %d' % (best_epoch,))
+
+
+fig,ax=plt.subplots(2,1,figsize=(12,8))
+ax[0].set_title('Finding optimum epochs- simple relu AE')
+ax[0].plot(history.epoch,val_acc_per_epoch)
+ax[0].set_xlabel('Number of epochs')
+ax[0].set_ylabel('MSE')
+ax[1].plot(history.epoch,val_acc_per_epoch)
+ax[1].set_xlabel('Number of latent dims')
+ax[1].set_ylabel('Best MSE in first 50 epochs')
+ax[1].set_yscale('log')
+loc = plticker.MultipleLocator(base=10.0) # this locator puts ticks at regular intervals
+ax[0].xaxis.set_major_locator(plticker.MultipleLocator(base=10.0))
+ax[0].xaxis.set_minor_locator(plticker.MultipleLocator(base=1.0))
+ax[1].xaxis.set_major_locator(plticker.MultipleLocator(base=10.0))
+ax[1].xaxis.set_minor_locator(plticker.MultipleLocator(base=1.0))
+plt.show()
+
+#%%Plot input vs output
+latent_space = ae_obj.encoder(ae_input_val).numpy()
+decoded_latent_space = ae_obj.decoder(latent_space)
+df_latent_space = pd.DataFrame(latent_space,index=df_all_data.index)
+
+fig,ax = plt.subplots(1)
+plt.scatter(ae_input_val,ae_obj.ae(ae_input_val))
+plt.title("AE input vs output")
+plt.xlabel('AE input')
+plt.ylabel('AE output')
+plt.show()
+
+
+#%%Plot latent space
+plt.scatter(latent_space[:,0],latent_space[:,1],c=)
+
+
+
+#%%See what biggest and smallest stdev features of latent space look like
+df_latent_space[df_latent_space.std().idxmax()].plot(kind='hist',title='Latent space, max stdev column')
+df_latent_space[df_latent_space.std().idxmin()].plot(kind='hist',title='Latent space, min stdev column')
+
+
+
+
+#%%AE top loss
+ds_AE_loss_per_sample = pd.Series(AE_calc_loss_per_sample(ae_obj.ae,ae_input_val,ae_input_val), index=df_all_data.index)
+plot_top_ae_loss(df_all_data,ds_AE_loss_per_sample)
+
+
+#%%Clustering
 
 
 
 
 
 
-#%%Merge the data frames
-df_all_data = pd.concat([df_Beijing_winter_data,df_Beijing_summer_data,df_Delhi_summer_data,df_Delhi_autumn_data],join='inner')
-df_all_err = pd.concat([df_Beijing_winter_err,df_Beijing_summer_err,df_Delhi_summer_err,df_Delhi_autumn_err],join='inner')
 
-plt.figure(figsize=(8,8))
-plt.scatter(df_all_err,df_all_data)
-
-#Make mz lookup
-df_Beijing_winter_mz = pd.DataFrame(Beijing_winter_mz).T.groupby([Beijing_winter_formula,Beijing_winter_RT],axis=1).aggregate("first")
-df_Beijing_summer_mz = pd.DataFrame(Beijing_summer_mz).T.groupby([Beijing_summer_formula,Beijing_summer_RT],axis=1).aggregate("first")
-df_Delhi_summer_mz = pd.DataFrame(Delhi_summer_mz).T.groupby([Delhi_summer_formula,Delhi_summer_RT],axis=1).aggregate("first")
-df_Delhi_autumn_mz = pd.DataFrame(Delhi_autumn_mz).T.groupby([Delhi_autumn_formula,Delhi_autumn_RT],axis=1).aggregate("first")
-ds_all_mz = pd.concat([df_Beijing_winter_mz,df_Beijing_summer_mz,df_Delhi_summer_mz,df_Delhi_autumn_mz],join='inner').iloc[0]
-
-#Sort 
-df_all_data.columns = ds_all_mz
-df_all_err.columns = ds_all_mz
-df_all_data.sort_index(axis=1,inplace=True)
-df_all_err.sort_index(axis=1,inplace=True)
-ds_all_mz.sort_values(inplace=True)
-df_all_data.columns = ds_all_mz.index
-df_all_err.columns = ds_all_mz.index
-
-plt.figure(figsize=(8,8))
-plt.scatter(df_all_err,df_all_data)
-
-#%%Make molecule lookup
-# ds_all_formula = pd.concat([Beijing_summer_formula,Beijing_winter_formula,Delhi_summer_formula,Delhi_autumn_formula])
-# ds_all_formula.index = np.concatenate([Beijing_summer_mz,Beijing_winter_mz,Delhi_summer_mz,Delhi_autumn_mz])
-# ds_all_formula = ds_all_formula[df_all_data.columns.get_level_values(0)]
-
-# df_all_formula = pd.DataFrame()
-# df_all_formula['formula'] = ds_all_formula.values
-# df_all_formula['mz'] = ds_all_formula.index
-# df_all_formula.drop_duplicates(inplace=True)
-
-
-# #ds_all_formula.drop_duplicates(inplace=True)
-# ds_all_formula = ds_all_formula.loc[ds_all_formula.index.drop_duplicates()]
+    
