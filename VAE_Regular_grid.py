62d4b21d78e99547c23bb980cd212a86aba966dc
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-10-14 16:57:15 +0100

    Initial commit

---
+++ VAE_Regular_grid.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 14 16:05:27 2022
+
+@author: mbcx5jt5
+"""
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
+from matplotlib.colors import ListedColormap
+import numpy as np
+import time
+
+import tensorflow.keras.backend as K
+from tensorflow.keras.callbacks import Callback, TerminateOnNaN
+import tensorflow as tf
+
+from sklearn.preprocessing import StandardScaler, FunctionTransformer, RobustScaler
+from sklearn.cluster import AgglomerativeClustering, DBSCAN, KMeans
+from sklearn.decomposition import PCA
+from sklearn_extra.cluster import KMedoids
+from sklearn.metrics import calinski_harabasz_score, davies_bouldin_score,silhouette_score,adjusted_rand_score
+from sklearn.manifold import TSNE
+
+import math
+
+
+import os
+os.chdir('C:/Work/Python/Github/Orbitrap_clustering')
+from ae_functions import *
+from orbitrap_functions import *
+
+
+
+
+
+#%%Load data from HDF
+filepath = r"C:\Users\mbcx5jt5\Google Drive\Shared_York_Man2\PMF data\ORBITRAP_Data_Pre_PMF.h5"
+df_all_data, df_all_err, ds_all_mz = Load_pre_PMF_data(filepath,join='outer')
+
+#Save data to CSV
+#df_all_data.to_csv(r"C:\Users\mbcx5jt5\Google Drive\Shared_York_Man2\PMF data\df_all_data.csv")
+#df_all_err.to_csv(r"C:\Users\mbcx5jt5\Google Drive\Shared_York_Man2\PMF data\df_all_err.csv")
+#ds_all_mz.to_csv(r"C:\Users\mbcx5jt5\Google Drive\Shared_York_Man2\PMF data\ds_all_mz.csv",index=False,header=False)
+
+#pd.DataFrame(df_all_data.columns.get_level_values(0),df_all_data.columns.get_level_values(1)).to_csv(r"C:\Users\mbcx5jt5\Google Drive\Shared_York_Man2\PMF data\RT_formula.csv",header=False)
+
+#Load all time data, ie start/mid/end
+df_all_times = pd.read_csv(r"C:\Users\mbcx5jt5\Google Drive\Shared_York_Man2\PMF data\Times_all.csv")
+df_all_times['date_start'] = pd.to_datetime(df_all_times['date_start'],dayfirst=True)
+df_all_times['date_mid'] = pd.to_datetime(df_all_times['date_mid'],dayfirst=True)
+df_all_times['date_end'] = pd.to_datetime(df_all_times['date_end'],dayfirst=True)
+
+df_all_times.set_index(df_all_times['date_mid'],inplace=True)
+fuzzy_index = pd.merge_asof(pd.DataFrame(index=df_all_data.index),df_all_times,left_index=True,right_index=True,direction='nearest',tolerance=pd.Timedelta(hours=1.25))
+df_all_times = df_all_times.loc[fuzzy_index['date_mid']]
+
+dataset_cat = delhi_beijing_datetime_cat(df_all_data)
+df_dataset_cat = pd.DataFrame(delhi_beijing_datetime_cat(df_all_data),columns=['dataset_cat'],index=df_all_data.index)
+ds_dataset_cat = df_dataset_cat['dataset_cat']
+
+time_cat = delhi_calc_time_cat(df_all_times)
+df_time_cat = pd.DataFrame(delhi_calc_time_cat(df_all_times),columns=['time_cat'],index=df_all_times.index)
+ds_time_cat = df_time_cat['time_cat']
+
+#This is a list of peaks with Sari's description from her PMF
+Sari_peaks_list = pd.read_csv(r'C:\Users\mbcx5jt5\Google Drive\Shared_York_Man2\Sari_Peaks_Sources.csv',index_col='Formula',na_filter=False)
+Sari_peaks_list = Sari_peaks_list[~Sari_peaks_list.index.duplicated(keep='first')]
+
+
+#%%Work out O:C, H:C, S:C, N:C ratios for all peaks
+#df_element_ratios = df_all_data.columns.get_level_values(0).to_series().apply(lambda x: chemform_ratios(x)[0])
+df_element_ratios = pd.DataFrame()
+df_element_ratios['H/C'] = df_all_data.columns.get_level_values(0).to_series().apply(lambda x: chemform_ratios(x)[0])
+df_element_ratios['O/C'] = df_all_data.columns.get_level_values(0).to_series().apply(lambda x: chemform_ratios(x)[1])
+df_element_ratios['N/C'] = df_all_data.columns.get_level_values(0).to_series().apply(lambda x: chemform_ratios(x)[2])
+df_element_ratios['S/C'] = df_all_data.columns.get_level_values(0).to_series().apply(lambda x: chemform_ratios(x)[3])
+
+
+#%%Transform data to regular mz grid
+mz_spacing = 1
+mz_grid = np.arange(round(ds_all_mz.min()),round(ds_all_mz.max())+mz_spacing,mz_spacing)
+df_all_data_grid = df_all_data.groupby(np.round(ds_all_mz), axis=1).sum()df_all_data.sum()
