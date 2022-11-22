7164a5d2602094941e6a38a52064d80b9962193a
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-11-22 12:07:28 +0000

    Add optimal_nclusters_r_card with test

---
+++ functions/optimal_nclusters_r_card.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import warnings
+
+
+
+
+import pdb
+
+
+def optimal_nclusters_r_card(nclusters,maxr,mincard,maxr_threshold=0.95,mincard_threshold=10):
+    """
+    Work out the optimal number of clusters based on the r and the cardinality (num points) of the smallest cluster
+
+    Parameters
+    ----------
+    nclusters : array
+        number of clusters.
+    maxr : array
+        maximum Pearson's R between clusters.    
+    mincard : array
+        cardinality (number of points) of smallest cluster.
+    maxr_threshold : variable, optional
+        threshold for maxr. The default is 0.95.
+    mincard_threshold : variable, optional
+        threshold for mincard. The default is 10.
+
+    Returns
+    -------
+    variable
+        Largest number of clusters for which (maxr > maxr_threshold) or neither (mincard < mincard_threshold).
+
+    """
+    #pdb.set_trace()
+    #maxr_over_threshold = np.where(maxr > maxr_threshold)
+    #mincard_under_threshold = mincard < mincard_threshold
+    #either_false = np.where(np.invert(np.invert(maxr_over_threshold) * np.invert(mincard_under_threshold)))
+    
+    
+    for index in range(len(nclusters)):
+        if maxr[index] > maxr_threshold or mincard[index] < mincard_threshold:
+            return nclusters[index]
+    
+    #If you reach this point, no data points above threshold
+    warnings.warn("optimal_nclusters_r_card() has not found any threshold-breaching values and is just returning the largest number of clusters from nclusters")
+    return np.max(nclusters)
--- functions/test_functions.py
+++ functions/test_functions.py
@@ -9,6 +9,7 @@ To run this file, run 'python -m pytest' from the main directory
 
 from functions.combine_multiindex import combine_multiindex
 from functions.prescale_whole_matrix import prescale_whole_matrix
+from functions.optimal_nclusters_r_card import optimal_nclusters_r_card
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -29,4 +30,18 @@ def test_prescale_whole_matrix():
     unscaled = np.array([[1,2,3],[4,5,6]]).T
     scaled, minmax = prescale_whole_matrix(unscaled,MinMaxScaler())
     assert scaled == pytest.approx(np.array([[0.,0.2,0.4],[0.6,0.8,1.0]]).T)
+    
+    
+def test_optimal_nclusters_r_card():
+    assert optimal_nclusters_r_card([1,2],[0.1,0.5],[50,2]) == 2
+    assert optimal_nclusters_r_card([1,2],[0.1,0.99],[50,40]) == 2
+    assert optimal_nclusters_r_card([1,2],[0.1,0.5],[50,40],maxr_threshold=0.05) == 1
+    assert optimal_nclusters_r_card([1,2],[0.1,0.5],[50,40],mincard_threshold=60) == 1
+    
+    #Warning if no suboptimal cluster numbers are found
+    with pytest.warns(UserWarning):
+        assert optimal_nclusters_r_card([1,2],[0.1,0.5],[50,40]) == 2
+        
+        
+        
     
