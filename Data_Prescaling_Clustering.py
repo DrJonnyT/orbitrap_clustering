3209ac5ef4b24fcfd22f16bdebb18ea7e70f9fcc
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-11-23 12:05:29 +0000

    Add element ratio bar charts

--- Data_Prescaling_Clustering.py
+++ Data_Prescaling_Clustering.py
@@ -33,6 +33,8 @@ from orbitrap_functions import *
 
 from functions.combine_multiindex import combine_multiindex
 from functions.prescale_whole_matrix import prescale_whole_matrix
+from functions.optimal_nclusters_r_card import optimal_nclusters_r_card
+from functions.avg_array_clusters import avg_array_clusters
 
 
 
@@ -84,7 +86,12 @@ df_element_ratios['S/C'] = df_all_data.columns.get_level_values(0).to_series().a
 
 
 
-
+#Work out time series of these variables in the unscaled data
+df_element_ratios_tseries = pd.DataFrame()
+df_element_ratios_tseries['H/C'] = df_all_data.set_axis(df_all_data.columns.get_level_values(0),axis=1).multiply(df_element_ratios['H/C'],axis=1).sum(axis=1) / df_all_data.sum(axis=1)
+df_element_ratios_tseries['O/C'] = df_all_data.set_axis(df_all_data.columns.get_level_values(0),axis=1).multiply(df_element_ratios['O/C'],axis=1).sum(axis=1) / df_all_data.sum(axis=1)
+df_element_ratios_tseries['N/C'] = df_all_data.set_axis(df_all_data.columns.get_level_values(0),axis=1).multiply(df_element_ratios['N/C'],axis=1).sum(axis=1) / df_all_data.sum(axis=1)
+df_element_ratios_tseries['S/C'] = df_all_data.set_axis(df_all_data.columns.get_level_values(0),axis=1).multiply(df_element_ratios['S/C'],axis=1).sum(axis=1) / df_all_data.sum(axis=1)
 
 
 
@@ -252,6 +259,36 @@ plt.tight_layout()
 plt.show()
 
 
+#Optimal num clusters based on R and min cardinality
+nclusters_unscaled = optimal_nclusters_r_card(df_cluster_labels_mtx_unscaled.columns.to_numpy(),df_cluster_corr_mtx_unscaled.max(axis=1),
+                             df_cluster_counts_mtx_unscaled.min(axis=1))
+
+nclusters_minmax = optimal_nclusters_r_card(df_cluster_labels_mtx_minmax.columns,df_cluster_corr_mtx_minmax.max(axis=1),
+                             df_cluster_counts_mtx_minmax.min(axis=1))
+
+nclusters_qt = optimal_nclusters_r_card(df_cluster_labels_mtx_qt.columns,df_cluster_corr_mtx_qt.max(axis=1),
+                             df_cluster_counts_mtx_qt.min(axis=1))
+
+nclusters_signoise = optimal_nclusters_r_card(df_cluster_labels_mtx_signoise.columns,df_cluster_corr_mtx_signoise.max(axis=1),
+                             df_cluster_counts_mtx_signoise.min(axis=1))
+
+#Optimal num clusters based just on R
+nclusters_unscaled = optimal_nclusters_r_card(df_cluster_labels_mtx_unscaled.columns,
+                                              df_cluster_corr_mtx_unscaled.max(axis=1),
+                                              df_cluster_counts_mtx_unscaled.min(axis=1),mincard_threshold=1)
+
+nclusters_minmax = optimal_nclusters_r_card(df_cluster_labels_mtx_minmax.columns,
+                                            df_cluster_corr_mtx_minmax.max(axis=1),
+                                            df_cluster_counts_mtx_minmax.min(axis=1),mincard_threshold=1)
+
+nclusters_qt = optimal_nclusters_r_card(df_cluster_labels_mtx_qt.columns,
+                                        df_cluster_corr_mtx_qt.max(axis=1),
+                                        df_cluster_counts_mtx_qt.min(axis=1),mincard_threshold=1)
+
+nclusters_signoise = optimal_nclusters_r_card(df_cluster_labels_mtx_signoise.columns,
+                                              df_cluster_corr_mtx_signoise.max(axis=1),
+                                              df_cluster_counts_mtx_signoise.min(axis=1),mincard_threshold=1)
+
 
 #%%Clustering workflow - unscaled data
 
@@ -287,7 +324,7 @@ cluster_profiles_mtx, cluster_profiles_mtx_norm, num_clusters_index, cluster_ind
 df_cluster_corr_mtx, df_prevcluster_corr_mtx = correlate_cluster_profiles(cluster_profiles_mtx_norm, num_clusters_index, cluster_index)
 plot_cluster_profile_corrs(df_cluster_corr_mtx, df_prevcluster_corr_mtx,suptitle='MinMax data HCA')
 
-plot_all_cluster_tseries_BeijingDelhi(df_cluster_labels_mtx,ds_dataset_cat,title_prefix='MinMaxdata HCA, ')
+plot_all_cluster_tseries_BeijingDelhi(df_cluster_labels_mtx,ds_dataset_cat,title_prefix='MinMax data HCA, ')
 
 plot_all_cluster_profiles(df_all_data,cluster_profiles_mtx_norm,num_clusters_index,ds_all_mz,df_clusters_HC_mtx,df_clusters_NC_mtx,df_clusters_OC_mtx,df_clusters_SC_mtx,
                           df_cluster_corr_mtx,df_prevcluster_corr_mtx,df_cluster_counts_mtx,Sari_peaks_list,title_prefix='MinMax data HCA, ')
@@ -296,6 +333,15 @@ df_clust_cat_counts, df_cat_clust_counts, df_clust_time_cat_counts,df_time_cat_c
     df_cluster_labels_mtx,ds_dataset_cat,ds_time_cat,title_prefix='MinMax data HCA, ',title_suffix='')
     
 compare_cluster_metrics(df_all_minmax,2,12,'agglom','MinMax data ',' metrics')
+
+
+
+#Plot the Minmax-scaled data
+cluster_profiles_mtx, cluster_profiles_mtx_norm, num_clusters_index, cluster_index = average_cluster_profiles(df_cluster_labels_mtx,df_all_minmax)
+df_cluster_corr_mtx, df_prevcluster_corr_mtx = correlate_cluster_profiles(cluster_profiles_mtx_norm, num_clusters_index, cluster_index)
+plot_all_cluster_profiles(df_all_minmax,cluster_profiles_mtx_norm,num_clusters_index,ds_all_mz,df_clusters_HC_mtx,df_clusters_NC_mtx,df_clusters_OC_mtx,df_clusters_SC_mtx,
+                          df_cluster_corr_mtx,df_prevcluster_corr_mtx,df_cluster_counts_mtx,Sari_peaks_list,title_prefix='MinMax data HCA, scaled data, ')
+
 
 #%%Clustering workflow - Quantile transformed data
 df_cluster_labels_mtx = cluster_n_times(df_all_qt,10,min_num_clusters=2,cluster_type='agglom')
@@ -312,11 +358,21 @@ plot_all_cluster_tseries_BeijingDelhi(df_cluster_labels_mtx,ds_dataset_cat,title
 
 plot_all_cluster_profiles(df_all_data,cluster_profiles_mtx_norm,num_clusters_index,ds_all_mz,df_clusters_HC_mtx,df_clusters_NC_mtx,df_clusters_OC_mtx,df_clusters_SC_mtx,
                           df_cluster_corr_mtx,df_prevcluster_corr_mtx,df_cluster_counts_mtx,Sari_peaks_list,title_prefix='QT data HCA, ')
-                        
+                       
 df_clust_cat_counts, df_cat_clust_counts, df_clust_time_cat_counts,df_time_cat_clust_counts = count_clusters_project_time(
     df_cluster_labels_mtx,ds_dataset_cat,ds_time_cat,title_prefix='QT data HCA, ',title_suffix='')
     
 compare_cluster_metrics(df_all_qt,2,12,'agglom','QT data ',' metrics')
+
+
+
+#Plot the QT-scaled data
+cluster_profiles_mtx, cluster_profiles_mtx_norm, num_clusters_index, cluster_index = average_cluster_profiles(df_cluster_labels_mtx,df_all_qt)
+df_cluster_corr_mtx, df_prevcluster_corr_mtx = correlate_cluster_profiles(cluster_profiles_mtx_norm, num_clusters_index, cluster_index)
+plot_all_cluster_profiles(df_all_qt,cluster_profiles_mtx_norm,num_clusters_index,ds_all_mz,df_clusters_HC_mtx,df_clusters_NC_mtx,df_clusters_OC_mtx,df_clusters_SC_mtx,
+                          df_cluster_corr_mtx,df_prevcluster_corr_mtx,df_cluster_counts_mtx,Sari_peaks_list,title_prefix='QT data HCA, scaled data, ')
+
+
 
 #%%Clustering workflow - Sig/noise transformed data
 df_cluster_labels_mtx = cluster_n_times(df_all_signoise,10,min_num_clusters=2,cluster_type='agglom')
@@ -339,3 +395,149 @@ df_clust_cat_counts, df_cat_clust_counts, df_clust_time_cat_counts,df_time_cat_c
     
 compare_cluster_metrics(df_all_signoise,2,12,'agglom','Sig/noise data ',' metrics')
 
+
+#Plot the QT-scaled data
+cluster_profiles_mtx, cluster_profiles_mtx_norm, num_clusters_index, cluster_index = average_cluster_profiles(df_cluster_labels_mtx,df_all_signoise)
+df_cluster_corr_mtx, df_prevcluster_corr_mtx = correlate_cluster_profiles(cluster_profiles_mtx_norm, num_clusters_index, cluster_index)
+plot_all_cluster_profiles(df_all_signoise,cluster_profiles_mtx_norm,num_clusters_index,ds_all_mz,df_clusters_HC_mtx,df_clusters_NC_mtx,df_clusters_OC_mtx,df_clusters_SC_mtx,
+                          df_cluster_corr_mtx,df_prevcluster_corr_mtx,df_cluster_counts_mtx,Sari_peaks_list,title_prefix='Sig/Noise data HCA, scaled data, ')
+
+
+
+
+
+#%%Now just doing the specified number of clusters
+cluster_labels_unscaled = AgglomerativeClustering(4).fit_predict(df_all_data)
+HC_clusters_unscaled = avg_array_clusters(cluster_labels_unscaled,df_element_ratios_tseries['H/C'],weights=df_all_data.sum(axis=1))
+NC_clusters_unscaled = avg_array_clusters(cluster_labels_unscaled,df_element_ratios_tseries['N/C'],weights=df_all_data.sum(axis=1))
+OC_clusters_unscaled = avg_array_clusters(cluster_labels_unscaled,df_element_ratios_tseries['O/C'],weights=df_all_data.sum(axis=1))
+SC_clusters_unscaled = avg_array_clusters(cluster_labels_unscaled,df_element_ratios_tseries['S/C'],weights=df_all_data.sum(axis=1))
+
+cluster_labels_minmax = AgglomerativeClustering(6).fit_predict(df_all_minmax)
+HC_clusters_minmax = avg_array_clusters(cluster_labels_minmax,df_element_ratios_tseries['H/C'],weights=df_all_data.sum(axis=1))
+NC_clusters_minmax = avg_array_clusters(cluster_labels_minmax,df_element_ratios_tseries['N/C'],weights=df_all_data.sum(axis=1))
+OC_clusters_minmax = avg_array_clusters(cluster_labels_minmax,df_element_ratios_tseries['O/C'],weights=df_all_data.sum(axis=1))
+SC_clusters_minmax = avg_array_clusters(cluster_labels_minmax,df_element_ratios_tseries['S/C'],weights=df_all_data.sum(axis=1))
+
+cluster_labels_qt = AgglomerativeClustering(6).fit_predict(df_all_qt)
+HC_clusters_qt = avg_array_clusters(cluster_labels_qt,df_element_ratios_tseries['H/C'],weights=df_all_data.sum(axis=1))
+NC_clusters_qt = avg_array_clusters(cluster_labels_qt,df_element_ratios_tseries['N/C'],weights=df_all_data.sum(axis=1))
+OC_clusters_qt = avg_array_clusters(cluster_labels_qt,df_element_ratios_tseries['O/C'],weights=df_all_data.sum(axis=1))
+SC_clusters_qt = avg_array_clusters(cluster_labels_qt,df_element_ratios_tseries['S/C'],weights=df_all_data.sum(axis=1))
+
+cluster_labels_signoise = AgglomerativeClustering(6).fit_predict(df_all_signoise)
+HC_clusters_signoise = avg_array_clusters(cluster_labels_signoise,df_element_ratios_tseries['H/C'],weights=df_all_data.sum(axis=1))
+NC_clusters_signoise = avg_array_clusters(cluster_labels_signoise,df_element_ratios_tseries['N/C'],weights=df_all_data.sum(axis=1))
+OC_clusters_signoise = avg_array_clusters(cluster_labels_signoise,df_element_ratios_tseries['O/C'],weights=df_all_data.sum(axis=1))
+SC_clusters_signoise = avg_array_clusters(cluster_labels_signoise,df_element_ratios_tseries['S/C'],weights=df_all_data.sum(axis=1))
+
+#Average value of each, across the whole dataset
+HC_all_avg = avg_array_clusters(np.zeros(len(df_element_ratios_tseries['H/C'])),df_element_ratios_tseries['H/C'],weights=df_all_data.sum(axis=1))[0]
+NC_all_avg = avg_array_clusters(np.zeros(len(df_element_ratios_tseries['N/C'])),df_element_ratios_tseries['N/C'],weights=df_all_data.sum(axis=1))[0]
+OC_all_avg = avg_array_clusters(np.zeros(len(df_element_ratios_tseries['O/C'])),df_element_ratios_tseries['O/C'],weights=df_all_data.sum(axis=1))[0]
+SC_all_avg = avg_array_clusters(np.zeros(len(df_element_ratios_tseries['S/C'])),df_element_ratios_tseries['S/C'],weights=df_all_data.sum(axis=1))[0]
+
+
+#%%Plot element ratio bar charts
+sns.set_context("talk", font_scale=1)
+
+#Make all the y scales the same
+HC_scale_max = 1.05 * np.amax([HC_clusters_unscaled.max(),HC_clusters_minmax.max(),HC_clusters_qt.max(),HC_clusters_signoise.max()])
+NC_scale_max = 1.05 * np.amax([NC_clusters_unscaled.max(),NC_clusters_minmax.max(),NC_clusters_qt.max(),NC_clusters_signoise.max()])
+OC_scale_max = 1.05 * np.amax([OC_clusters_unscaled.max(),OC_clusters_minmax.max(),OC_clusters_qt.max(),OC_clusters_signoise.max()])
+SC_scale_max = 1.05 * np.amax([SC_clusters_unscaled.max(),SC_clusters_minmax.max(),SC_clusters_qt.max(),SC_clusters_signoise.max()])
+
+fig,ax = plt.subplots(2,2,figsize=(10,10))
+ax = ax.ravel()
+ax[0].bar(HC_clusters_unscaled.index.astype(str),HC_clusters_unscaled.values,color='gray')
+ax[0].set_ylabel('H/C')
+ax[0].set_ylim(None,HC_scale_max)
+ax[0].axhline(y=HC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[1].bar(NC_clusters_unscaled.index.astype(str),NC_clusters_unscaled.values)
+ax[1].set_ylabel('N/C')
+ax[1].set_ylim(None,NC_scale_max)
+ax[1].axhline(y=NC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[2].bar(OC_clusters_unscaled.index.astype(str),OC_clusters_unscaled.values,color='g')
+ax[2].set_ylabel('O/C')
+ax[2].set_ylim(None,OC_scale_max)
+ax[2].axhline(y=OC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[3].bar(SC_clusters_unscaled.index.astype(str),SC_clusters_unscaled.values,color='r')
+ax[3].set_ylabel('S/C')
+ax[3].set_ylim(None,SC_scale_max)
+ax[3].axhline(y=SC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+plt.suptitle('Unscaled data, 4 clusters')
+plt.tight_layout()
+plt.show()
+
+fig,ax = plt.subplots(2,2,figsize=(10,10))
+ax = ax.ravel()
+ax[0].bar(HC_clusters_minmax.index.astype(str),HC_clusters_minmax.values,color='gray')
+ax[0].set_ylabel('H/C')
+ax[0].set_ylabel('H/C')
+ax[0].set_ylim(None,HC_scale_max)
+ax[0].axhline(y=HC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[1].bar(NC_clusters_minmax.index.astype(str),NC_clusters_minmax.values)
+ax[1].set_ylabel('N/C')
+ax[1].set_ylim(None,NC_scale_max)
+ax[1].axhline(y=NC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[2].bar(OC_clusters_minmax.index.astype(str),OC_clusters_minmax.values,color='g')
+ax[2].set_ylabel('O/C')
+ax[2].set_ylim(None,OC_scale_max)
+ax[2].axhline(y=OC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[3].bar(SC_clusters_minmax.index.astype(str),SC_clusters_minmax.values,color='r')
+ax[3].set_ylabel('S/C')
+ax[3].set_ylim(None,SC_scale_max)
+ax[3].axhline(y=SC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+plt.suptitle('MinMax data, 6 clusters')
+plt.tight_layout()
+plt.show()
+
+fig,ax = plt.subplots(2,2,figsize=(10,10))
+ax = ax.ravel()
+ax[0].bar(HC_clusters_qt.index.astype(str),HC_clusters_qt.values,color='gray')
+ax[0].set_ylabel('H/C')
+ax[0].set_ylabel('H/C')
+ax[0].set_ylim(None,HC_scale_max)
+ax[0].axhline(y=HC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[1].bar(NC_clusters_qt.index.astype(str),NC_clusters_qt.values)
+ax[1].set_ylabel('N/C')
+ax[1].set_ylim(None,NC_scale_max)
+ax[1].axhline(y=NC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[2].bar(OC_clusters_qt.index.astype(str),OC_clusters_qt.values,color='g')
+ax[2].set_ylabel('O/C')
+ax[2].set_ylim(None,OC_scale_max)
+ax[2].axhline(y=OC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[3].bar(SC_clusters_qt.index.astype(str),SC_clusters_qt.values,color='r')
+ax[3].set_ylabel('S/C')
+ax[3].set_ylim(None,SC_scale_max)
+ax[3].axhline(y=SC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+plt.suptitle('QT data, 6 clusters')
+plt.tight_layout()
+plt.show()
+
+fig,ax = plt.subplots(2,2,figsize=(10,10))
+ax = ax.ravel()
+ax[0].bar(HC_clusters_signoise.index.astype(str),HC_clusters_signoise.values,color='gray')
+ax[0].set_ylabel('H/C')
+ax[0].set_ylabel('H/C')
+ax[0].set_ylim(None,HC_scale_max)
+ax[0].axhline(y=HC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[1].bar(NC_clusters_signoise.index.astype(str),NC_clusters_signoise.values)
+ax[1].set_ylabel('N/C')
+ax[1].set_ylim(None,NC_scale_max)
+ax[1].axhline(y=NC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[2].bar(OC_clusters_signoise.index.astype(str),OC_clusters_signoise.values,color='g')
+ax[2].set_ylabel('O/C')
+ax[2].set_ylim(None,OC_scale_max)
+ax[2].axhline(y=OC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+ax[3].bar(SC_clusters_signoise.index.astype(str),SC_clusters_signoise.values,color='r')
+ax[3].set_ylabel('S/C')
+ax[3].set_ylim(None,SC_scale_max)
+ax[3].axhline(y=SC_all_avg, linestyle='--',color=(0, 0, 0, 0.85))
+plt.suptitle('Sig/Noise data, 6 clusters')
+plt.tight_layout()
+plt.show()
+
+sns.reset_orig()
+    
+    
