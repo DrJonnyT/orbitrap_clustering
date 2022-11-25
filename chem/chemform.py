172f10b2354e615a9c16592ad3d3dd1230bfbc34
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-11-25 10:09:56 +0000

    Create file with test for chemform

--- chem/chemform.py
+++ chem/chemform.py
@@ -10,27 +10,27 @@ class chemform:
         try:
             self.C = int(re.findall(r'C(\d+)',formula)[0])
         except:
-                self.C = len(re.findall(r'C',formula))
+            self.C = len(re.findall(r'C',formula))
         
         try:
             self.H = int(re.findall(r'H(\d+)',formula)[0])
         except:
-                self.H = len(re.findall(r'H',formula))
+            self.H = len(re.findall(r'H',formula))
     
         try:
             self.O = int(re.findall(r'O(\d+)',formula)[0])
         except:
-                self.O = len(re.findall(r'O',formula))
+            self.O = len(re.findall(r'O',formula))
         
         try:
             self.S = int(re.findall(r'S(\d+)',formula)[0])
         except:
-                self.S = len(re.findall(r'S',formula))
+            self.S = len(re.findall(r'S',formula))
         
         try:
             self.N = int(re.findall(r'N(\d+)',formula)[0])
         except:
-                self.N = len(re.findall(r'N',formula))
+            self.N = len(re.findall(r'N',formula))
         
     
         
---
+++ chem/test_chem.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from chem.chemform import chemform
+
+def test_chemform():
+    chem1 = chemform('CH2O3S')
+    assert chem1.C == 1
+    assert chem1.H == 2
+    assert chem1.O == 3
+    assert chem1.S == 1
+    
+    chem2 = chemform('C H O2')
+    assert chem2.H == 1
+    assert chem2.O == 2
+    assert chem2.S == 0
+    
+    
+    
+
