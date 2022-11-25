758aa0073fdc3236890a062e8ede4323a4918392
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-11-25 10:01:13 +0000

    Create file

---
+++ chem/chemform.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import re
+
+class chemform:
+    """
+    A class for chemical formulae
+    """
+    def __init__(self, formula):
+    
+        try:
+            self.C = int(re.findall(r'C(\d+)',formula)[0])
+        except:
+                self.C = len(re.findall(r'C',formula))
+        
+        try:
+            self.H = int(re.findall(r'H(\d+)',formula)[0])
+        except:
+                self.H = len(re.findall(r'H',formula))
+    
+        try:
+            self.O = int(re.findall(r'O(\d+)',formula)[0])
+        except:
+                self.O = len(re.findall(r'O',formula))
+        
+        try:
+            self.S = int(re.findall(r'S(\d+)',formula)[0])
+        except:
+                self.S = len(re.findall(r'S',formula))
+        
+        try:
+            self.N = int(re.findall(r'N(\d+)',formula)[0])
+        except:
+                self.N = len(re.findall(r'N',formula))
+        
+    
+        
