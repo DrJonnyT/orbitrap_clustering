3d1522390868ddf95820631cb63c318e5ec9d138
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-11-29 17:08:43 +0000

    Add basic working plot_cluster_heatmap_BeijingDelhi

--- plotting/beijingdelhi.py
+++ plotting/beijingdelhi.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from matplotlib.ticker import MaxNLocator
-
+from matplotlib.ticker import MaxNLocator, IndexLocator
+import numpy as np
+import pandas as pd
+import pdb
 
 
 def plot_all_cluster_tseries_BeijingDelhi(df_cluster_labels_mtx,ds_dataset_cat, title_prefix='',title_suffix=''):
@@ -66,6 +68,103 @@ def plot_tseries_BeijingDelhi(c,ds_dataset_cat,suptitle,ylabel,integer_labels=Fa
     fig.tight_layout()
     plt.show()
     
+    
+    
+    
+#%% Plot the time series divided into 4 projects
+#c is the time series of cluster index
+#ds_dataset_cat is the categorical data series of which dataset there is
+#suptitle is the title to go at the top of the plot
+def plot_cluster_heatmap_BeijingDelhi(c,ds_dataset_cat,suptitle,ylabel,integer_labels=False):
+    fig,ax = plt.subplots(2,2,figsize=(9,9))
+    ax=ax.ravel()
+    
+    myFmt = mdates.DateFormatter('%d/%m/%y')
+    
+    for idx in range(4):
+        cat = ds_dataset_cat.unique()[idx]
+        c_thiscat = c[ds_dataset_cat == cat]
+        ds_dataset_cat_thiscat = ds_dataset_cat[ds_dataset_cat == cat]
+    
+        xbins = pd.date_range(ds_dataset_cat_thiscat.index.floor("1D").min(),ds_dataset_cat_thiscat.index.ceil("1D").max(),freq="1D") 
+        ybins = np.unique(c)
+        #pdb.set_trace()
+        #ax[idx].yaxis.set_major_locator(MaxNLocator(integer=True))
+        ybins= np.append(ybins,ybins.max() + 1) -0.5
+        ax[idx].hist2d(ds_dataset_cat_thiscat.index,c_thiscat, bins=[xbins,ybins])
+        ax[idx].set_yticks(np.unique(c))
+        
+        #Format dates on x labels
+        ax[idx].xaxis.set_major_formatter(myFmt)
+        ax[idx].tick_params(axis='x', labelrotation=45)
+        
+        #Move y axis labels slightly
+        #ax[idx].set_yticks(np.unique(c_thiscat) + 0.5)
+        #ax[idx].yaxis.set_label_coords(0,0.5)
+        
+        ax[idx].set_ylabel(ylabel)
+        ax[idx].set_title(cat)
+        
+        ax[idx].xaxis_date()
+        
+        #Set x tick locations
+        myLocator = IndexLocator( base = 7, offset = 0)
+        ax[idx].xaxis.set_major_locator(myLocator)
+        
+        #Add grid
+        ax[idx].grid(axis='x',linestyle='--',alpha=0.6)
+        
+        
+        
+    
+    
+    
+    # ax0=ax[0]
+    # ax0.plot(ds_dataset_cat.index,c)
+    # ax0.set_xlim(ds_dataset_cat[ds_dataset_cat == "Beijing_winter"].index.min(),ds_dataset_cat[ds_dataset_cat == "Beijing_winter"].index.max())
+    # ax0.set_title('Beijing winter')
+    # ax0.set_ylabel(ylabel)
+
+    # ax1=ax[1]
+    # ax1.plot(ds_dataset_cat.index,c)
+    # ax1.set_xlim(ds_dataset_cat[ds_dataset_cat == "Beijing_summer"].index.min(),ds_dataset_cat[ds_dataset_cat == "Beijing_summer"].index.max())
+    # ax1.set_title('Beijing summer')
+    # ax1.set_ylabel(ylabel)
+
+    # ax2=ax[2]
+    # ax2.plot(ds_dataset_cat.index,c)
+    # ax2.set_xlim(ds_dataset_cat[ds_dataset_cat == "Delhi_summer"].index.min(),ds_dataset_cat[ds_dataset_cat == "Delhi_summer"].index.max())
+    # ax2.set_title('Delhi summer')
+    # ax2.set_ylabel(ylabel)
+
+    # ax3=ax[3]
+    # ax3.plot(ds_dataset_cat.index,c)
+    # ax3.set_xlim(ds_dataset_cat[ds_dataset_cat == "Delhi_autumn"].index.min(),ds_dataset_cat[ds_dataset_cat == "Delhi_autumn"].index.max())
+    # ax3.set_title('Delhi autumn')
+    # ax3.set_ylabel(ylabel)
+
+    # #x ticks
+    # myFmt = mdates.DateFormatter('%d/%m')
+    # ax0.xaxis.set_major_formatter(myFmt)
+    # ax1.xaxis.set_major_formatter(myFmt)
+    # ax2.xaxis.set_major_formatter(myFmt)
+    # ax3.xaxis.set_major_formatter(myFmt)
+    # ax0.tick_params(axis='x', labelrotation=45)
+    # ax1.tick_params(axis='x', labelrotation=45)
+    # ax2.tick_params(axis='x', labelrotation=45)
+    # ax3.tick_params(axis='x', labelrotation=45)
+    
+    # #y ticks
+    # if(integer_labels == True):
+    #     ax0.yaxis.set_major_locator(MaxNLocator(integer=True))
+    #     ax1.yaxis.set_major_locator(MaxNLocator(integer=True))
+    #     ax2.yaxis.set_major_locator(MaxNLocator(integer=True))
+    #     ax3.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    fig.suptitle(suptitle)
+    fig.subplots_adjust(top=0.88)
+    fig.tight_layout()
+    plt.show()
     
 #%% Plot the time series divided into 4 projects
 #c is the time series of cluster index
