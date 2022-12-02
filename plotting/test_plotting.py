fb5d491fb3a1bcb07d2e13764ca016b08964a802
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-12-02 11:01:56 +0000

    Add nbins argument to cmap_EOS11

--- plotting/test_plotting.py
+++ plotting/test_plotting.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from plotting.cmap_EOS11 import cmap_EOS11
 
+from matplotlib.colors import LinearSegmentedColormap
+
 def test_cmap_EOS11():
     cmap = cmap_EOS11()
+    assert type(cmap) == LinearSegmentedColormap
+    assert cmap.N == 11
+    assert cmap_EOS11(50).N == 50
     
