f5ac6b1cdcd537125c1c37b21338d084e761fdb2
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-12-05 16:21:10 +0000

    Add molecule_type_pos_frac_clusters with basic test

--- clustering/molecule_type_pos_frac.py
+++ clustering/molecule_type_pos_frac.py
@@ -29,33 +29,49 @@ def molecule_type_pos_frac(mass_spectrum,molecule_types,**kwargs):
         mols_list = ['CHN', 'CHNS', 'CHO', 'CHON', 'CHONS', 'CHOS', 'CHS']
     
     
-    df_output = pd.Series(index=mols_list,dtype='float')
+    ds_output = pd.Series(index=mols_list,dtype='float')
     
     #Loop through all the different molecules
     for molecule in mols_list:
         molecule_data = mass_spectrum[np.array(molecule_types) == molecule]
         #pdb.set_trace()
-        df_output.loc[molecule] = np.greater_equal(molecule_data,0).mean()
+        ds_output.loc[molecule] = np.greater_equal(molecule_data,0).mean()
+            
+    return ds_output
+
+
+
+
+
+def molecule_type_pos_frac_clusters(qt_data_2D,molecule_types,cluster_labels_1D,**kwargs):
+    """
+    Loop through molecule_type_pos_frac, selecting different clusters
+    """
+    
+    #Use custom list of molecules if required
+    if 'mols_list' in kwargs:
+        mols_list = kwargs.get('mols_list')
+    else:
+        #Default list of molecule types
+        mols_list = ['CHN', 'CHNS', 'CHO', 'CHON', 'CHONS', 'CHOS', 'CHS']
+    
+    
+    df_output = pd.DataFrame(index=np.unique(cluster_labels_1D),columns=mols_list)
+    
+    for cluster in df_output.index:
+       # pdb.set_trace()
+        data_this_cluster = qt_data_2D[np.array(cluster_labels_1D) == cluster]
+
+        #Loop through all the different molecules
+        for molecule in mols_list:
+            #pdb.set_trace()
+            molecule_data = data_this_cluster.T[np.array(molecule_types) == molecule]
+            df_output.loc[cluster][molecule] = np.greater_equal(molecule_data,0).mean()
+            
+
+
             
     return df_output
-
-
-
-
-
-def molecule_type_pos_frac_clusters(cluster_labels_qt,qt_data,molecule_types):
-    
-#     df_output = pd.DataFrame(index=np.unique(cluster_labels_qt),columns=np.unique(molecule_types))
-    
-#     for cluster in df_output.index:
-#         data_this_cluster = qt_data[cluster_labels_qt == cluster]
-#         #Loop through all the different molecules
-#         for molecule in df_output.columns:
-#             this_molecule_this_cluster = data_this_cluster[molecule_types == molecule]
-#             pdb.set_trace()
-#             df_output.loc[cluster][molecule] = np.greater_equal(this_molecule_this_cluster,0).mean()
-            
-#     return df_output
     
     
     
--- clustering/test_clustering.py
+++ clustering/test_clustering.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from clustering import molecule_type_pos_frac
+from clustering.molecule_type_pos_frac import molecule_type_pos_frac_clusters
 
 
 def test_molecule_type_pos_frac():
@@ -8,4 +9,15 @@ def test_molecule_type_pos_frac():
     data = np.array([1,1,-1,1,0,-1])
     mols_list = np.unique(mol_types)
     assert np.array_equal( molecule_type_pos_frac(data,mol_types,mols_list=mols_list).to_numpy(), [1,0.5,1,0])
+    
+    
+def test_molecule_type_pos_frac_clusters():
+    mol_types = ['CHO','CHO','CHON','CHON','CHONS','CHOS']
+    data = np.array([[1,1,-1,1,0,-1],[-1,-1,-1,1,0,-1]])
+    mols_list = np.unique(mol_types)
+    clusters = [0,1]
+    
+    assert np.array_equal( molecule_type_pos_frac_clusters(data,mol_types,clusters,mols_list=mols_list).to_numpy(), 
+                          np.array([[1,0.5,1,0],
+                          [0,0.5,1,0]]))
     
