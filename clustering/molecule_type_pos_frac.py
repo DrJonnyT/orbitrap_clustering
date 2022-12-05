c3700ad5eeb5cc1cdd0d0cd90b95f6e54f7f0081
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-12-05 17:20:01 +0000

    Add molecule_type_pos_frac_clusters_mtx

--- clustering/molecule_type_pos_frac.py
+++ clustering/molecule_type_pos_frac.py
@@ -43,9 +43,9 @@ def molecule_type_pos_frac(mass_spectrum,molecule_types,**kwargs):
 
 
 
-def molecule_type_pos_frac_clusters(qt_data_2D,molecule_types,cluster_labels_1D,**kwargs):
+def molecule_type_pos_frac_clusters(data_2D,molecule_types,cluster_labels_1D,**kwargs):
     """
-    Loop through molecule_type_pos_frac, selecting different clusters
+    Like molecule_type_pos_frac, but selecting data from different clusters
     """
     
     #Use custom list of molecules if required
@@ -60,18 +60,53 @@ def molecule_type_pos_frac_clusters(qt_data_2D,molecule_types,cluster_labels_1D,
     
     for cluster in df_output.index:
        # pdb.set_trace()
-        data_this_cluster = qt_data_2D[np.array(cluster_labels_1D) == cluster]
+        data_this_cluster = data_2D[np.array(cluster_labels_1D) == cluster]
 
         #Loop through all the different molecules
         for molecule in mols_list:
             #pdb.set_trace()
-            molecule_data = data_this_cluster.T[np.array(molecule_types) == molecule]
+            molecule_data = data_this_cluster.T[np.array(molecule_types) == molecule].to_numpy()
             df_output.loc[cluster][molecule] = np.greater_equal(molecule_data,0).mean()
-            
-
-
-            
+                        
     return df_output
+
+
+def molecule_type_pos_frac_clusters_mtx(data_2D,molecule_types,df_cluster_label_mtx,**kwargs):
+    """
+    Loop through molecule_type_pos_frac_clusters, taking cluster labels from a 2D matrix
     
+    Parameters
+    ----------
+    see molecule_type_pos_frac_clusters
     
+    df_cluster_label_mtx: pandas dataframe
+    index is different samples, like the index of data_2D
+    columns is the number of clusters
+    
+    Returns
+    -------
+    output_array: array
+    an array of dataframes, each of which is an output of molecule_type_pos_frac_clusters
+    
+    num_clusters: array
+    the corresponding number of clusters
+
+    """
+    
+    #Use custom list of molecules if required
+    if 'mols_list' in kwargs:
+        mols_list = kwargs.get('mols_list')
+    else:
+        #Default list of molecule types
+        mols_list = ['CHN', 'CHNS', 'CHO', 'CHON', 'CHONS', 'CHOS', 'CHS']
+        
+        
+    num_clusters = df_cluster_label_mtx.columns
+    output_array = []    
+    
+    for n_clusters in num_clusters:
+        #pdb.set_trace()
+        output_array.append(molecule_type_pos_frac_clusters(data_2D,molecule_types,df_cluster_label_mtx[n_clusters],mols_list=mols_list))
+        
+    return output_array, num_clusters
     
