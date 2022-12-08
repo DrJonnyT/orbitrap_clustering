0144229746b26e766604d45c0aa5435c24f1df71
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2022-12-08 17:20:58 +0000

    Refactored correlate_cluster_profiles

--- orbitrap_functions.py
+++ orbitrap_functions.py
@@ -966,43 +966,7 @@ def average_cluster_profiles(df_cluster_labels_mtx,df_data):
     return cluster_profiles_mtx, cluster_profiles_mtx_norm, num_clusters_index, cluster_index
 
 
-#%%Correlate cluster mass spectral profiles
 
-def correlate_cluster_profiles(cluster_profiles_mtx_norm, num_clusters_index, cluster_index):
-    
-    df_cluster_corr_mtx = pd.DataFrame(np.NaN, index = num_clusters_index, columns = cluster_index)
-    df_prevcluster_corr_mtx = pd.DataFrame(np.NaN, index = num_clusters_index, columns = cluster_index)
-    
-
-    #index is the number of clusters
-    #columns is the cluster in question
-    for x_idx in np.arange(cluster_profiles_mtx_norm.shape[0]):
-        num_clusters = num_clusters_index[x_idx]
-        #print(num_clusters)
-        if(num_clusters>1):
-            for this_cluster in np.arange(num_clusters):
-                #Find correlations with other clusters from the same num_clusters
-                this_cluster_profile = cluster_profiles_mtx_norm[x_idx,this_cluster,:]  
-                other_clusters_profiles = cluster_profiles_mtx_norm[x_idx,cluster_index!=this_cluster,:]
-                profiles_corr = np.zeros(other_clusters_profiles.shape[0])
-                for y_idx in np.arange(num_clusters-1):
-                    this_other_cluster_profile = other_clusters_profiles[y_idx,:]
-                    profiles_corr[y_idx] = pearsonr(this_cluster_profile,this_other_cluster_profile)[0]
-                df_cluster_corr_mtx.loc[num_clusters,this_cluster] = profiles_corr.max()
-        if(num_clusters>1 and x_idx > 0):
-            for this_cluster in np.arange(num_clusters):
-                #Find correlations with the clusters from the previous num_clusters
-                this_cluster_profile = cluster_profiles_mtx_norm[x_idx,this_cluster,:]  
-                prev_clusters_profiles = cluster_profiles_mtx_norm[x_idx-1,:,:]
-                profiles_corr = np.zeros(prev_clusters_profiles.shape[0])
-                for y_idx in np.arange(num_clusters-1):
-                    this_prev_cluster_profile = prev_clusters_profiles[y_idx,:]
-                    #pdb.set_trace()
-                    profiles_corr[y_idx] = pearsonr(this_cluster_profile,this_prev_cluster_profile)[0]
-                df_prevcluster_corr_mtx.loc[num_clusters,this_cluster] = profiles_corr.max()
-            
-            
-    return df_cluster_corr_mtx, df_prevcluster_corr_mtx
 
 
 #%%Plot cluster profile correlations
