7a2f818e37ecf9176828982db114d3003d5d64e5
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2023-01-23 17:02:21 +0000

    Pick out unusually high/low peaks and save to csv

--- Data_Prescaling_Clustering.py
+++ Data_Prescaling_Clustering.py
@@ -42,6 +42,7 @@ from file_loaders.load_pre_PMF_data import load_pre_PMF_data
 from clustering.molecule_type_math import molecule_type_pos_frac_clusters_mtx
 from clustering.cluster_n_times import cluster_n_times, cluster_n_times_fn
 from clustering.correlate_cluster_profiles import correlate_cluster_profiles
+from clustering.cluster_top_percentiles import cluster_top_percentiles
 from plotting.compare_cluster_metrics import compare_cluster_metrics, compare_cluster_metrics_fn
 from plotting.plot_binned_mol_data import bin_mol_data_for_plot, plot_binned_mol_data
 from plotting.plot_cluster_profiles import plot_all_cluster_profiles
@@ -1004,75 +1005,25 @@ plot_cluster_aerosolomics_spectra(cluster_labels_qt,df_all_aerosolomics,suptitle
 
 #%%Extract the most unusually high and low molecules for each cluster
 
-from scipy.stats import percentileofscore
-
-def cluster_top_percentiles(df_data,cluster_labels,num,highest=True,dropRT=False):
-    """
-    For each cluster label, extract a list of the top num peaks that are unusually high or low.
-    This means peaks that have a high/low percentile within their entire distribution
-
-    Parameters
-    ----------
-    df_data : Pandas DataFrame
-        DataFrame of orbitrap filter data. Index is normally time, columns are peak labels, z is concentration
-    cluster_labels : numpy array
-        Time series of cluster labels, with the same time basis as df_data
-    num : int
-        Number of peaks to extract
-    highest : bool, optional
-        Pick out the num highest peaks. The default is True. If False, it picks out the num lowest peaks.
-    dropRT : bool, optional
-        If True, return only the molecular formula. If False, return also the retention time (RT). The default is False.
-
-    Returns
-    -------
-    df_top_pct : Pandas DataFrame
-        DataFrame containing the molecule names of the top/bottom num peaks, as the relevant column label from df_data.
-
-    """
-    unique_labels = np.unique(cluster_labels)
-    num_clust = len(unique_labels)
-    
-    df_top_pct_labels = pd.DataFrame(columns=unique_labels)
-    df_top_pct_labels.columns.rename('cluster',inplace=True)
-    
-    df_top_pct_values = pd.DataFrame(columns=unique_labels)
-    df_top_pct_values.columns.rename('cluster',inplace=True)
-    
-    for cluster in unique_labels:
-        data_thisclust = df_data.loc[cluster_labels==cluster]
-        
-        ds_pct = pd.Series([percentileofscore(df_data[mol],data_thisclust[mol].median()) for mol in df_data.columns],index=df_data.columns, dtype='float')
-        
-        #Extract the top num peaks
-        if(highest):
-            ds_pct_top = ds_pct.sort_values(ascending=False).iloc[0:num]
-        else:
-            ds_pct_top = ds_pct.sort_values(ascending=True).iloc[0:num]
-        
-        #pdb.set_trace()
-        
-        if(dropRT):
-            df_top_pct_labels[cluster] = ds_pct_top.index.get_level_values(0)
-        else:
-            df_top_pct_labels[cluster] = ds_pct_top.index.values
-        
-        df_top_pct_values[cluster] = ds_pct_top.values
-        pdb.set_trace()
-        df_top_pct_values[cluster] = ds_pct_top
-        
-    return df_top_pct_labels, df_top_pct_values
-
-
 #Calculate the top30 unusually high or low peaks in each cluster
-top_peaks_unscaled, top_peaks_unscaled_vals = cluster_top_percentiles(df_all_data,cluster_labels_unscaled,30,dropRT=True)
-top_peaks_qt, top_peaks_qt_vals = cluster_top_percentiles(df_all_data,cluster_labels_qt,30,dropRT=True)
-top_peaks_normdot, top_peaks_normdot_vals = cluster_top_percentiles(df_all_data,cluster_labels_normdot,30,dropRT=True)
+top_peaks_unscaled = cluster_top_percentiles(df_all_data,cluster_labels_unscaled,30)
+top_peaks_qt = cluster_top_percentiles(df_all_data,cluster_labels_qt,30)
+top_peaks_normdot= cluster_top_percentiles(df_all_data,cluster_labels_normdot,30)
 
-bottom_peaks_unscaled, bottom_peaks_unscaled_vals = cluster_top_percentiles(df_all_data,cluster_labels_unscaled,30,dropRT=True,highest=False)
-bottom_peaks_qt, bottom_peaks_qt_vals = cluster_top_percentiles(df_all_data,cluster_labels_qt,30,dropRT=True,highest=False)
-bottom_peaks_normdot, bottom_peaks_normdot_vals = cluster_top_percentiles(df_all_data,cluster_labels_normdot,30,dropRT=True,highest=False)
+bottom_peaks_unscaled= cluster_top_percentiles(df_all_data,cluster_labels_unscaled,30,highest=False)
+bottom_peaks_qt= cluster_top_percentiles(df_all_data,cluster_labels_qt,30,highest=False)
+bottom_peaks_normdot= cluster_top_percentiles(df_all_data,cluster_labels_normdot,30,highest=False)
 
+#Export to CSV
+export_path = r'C:\Users\mbcx5jt5\Dropbox (The University of Manchester)\Complex-SOA\Clustering\Unusual_Peaks'
+
+top_peaks_unscaled.to_csv(export_path + '\pct_top_peaks_unscaled.csv')
+top_peaks_qt.to_csv(export_path + '\pct_top_peaks_qt.csv')
+top_peaks_normdot.to_csv(export_path + '\pct_top_peaks_normdot.csv')
+
+bottom_peaks_unscaled.to_csv(export_path + '\pct_bottom_peaks_unscaled.csv')
+bottom_peaks_qt.to_csv(export_path + '\pct_bottom_peaks_qt.csv')
+bottom_peaks_normdot.to_csv(export_path + '\pct_bottom_peaks_normdot.csv')
 
 #%%A big gap
 ##Everything below here is old and probably wont make it into the final script
