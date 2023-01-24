a4992005e10ee2d5dc65a77d54faabfbc949aa61
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2023-01-24 11:18:32 +0000

    Improve rounding

--- clustering/cluster_top_percentiles.py
+++ clustering/cluster_top_percentiles.py
@@ -52,6 +52,10 @@ def cluster_top_percentiles(df_data,cluster_labels,num,highest=True,dropRT=False
             df_top_pct[str(cluster) + "_compound"] = ds_pct_top.index.get_level_values(0)
         else:
             df_top_pct[str(cluster) + "_compound"] = "(" + combine_multiindex(ds_pct_top.index,nospaces=True) + ")"
-        df_top_pct[str(cluster) + "_pct"] = ds_pct_top.values.round(1)
+        
+        #Round to 1DP
+        ds_pct_top.loc[:] = ["%.1f" % value for value in ds_pct_top.values]
+        
+        df_top_pct[str(cluster) + "_pct"] = ds_pct_top.values
         
     return df_top_pct
